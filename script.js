6eeec618a9ac16621ceb0db4f503e1ffeef2ffc1
LuisaPereira <luisapereirauff@gmail.com>
2026-03-18 17:30:52 -0300

    name change

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/Avatar-Lu-Light.png")
+    img.setAttribute("src", "./assets/avatar-lu-light.png")
   } else {
-    img.setAttribute("src", "./assets/Avatar-Lu.png")
+    img.setAttribute("src", "./assets/avatar-lu.png")
   }
 }
